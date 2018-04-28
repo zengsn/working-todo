151e56e79e00b60d012477bcc878ab6bafcb0b04
zengsn <shaoning.zeng@gmail.com>
2018-04-28 15:16:19 +0800

    try 1st todo on github

---
+++ 20180428-create_butterfly_tf_record.py.js
@@ -0,0 +1,7 @@
+/**
+ * @todo Create ButterFly dataset for object detection
+ * @body Reseearch on Object Detection.
+ */
+function create_butterfly_tf_record_py () {
+  // based on create_pascal_tf_record.py in tensorflow/models
+}
